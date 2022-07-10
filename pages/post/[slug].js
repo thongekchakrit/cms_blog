f4e107d38426947ca934332bda33e19f6b9f4046
Chakrit Thong Ek <thongekchakrit@gmail.com>
2022-07-10 18:50:57 +0800

    Updated Loader

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { getPosts, getPostDetails } from '../../services';
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader} from '../../components'
 import { useRouter } from 'next/router';
 
 const PostDetails = ( { post }) => {
@@ -9,6 +9,7 @@ const PostDetails = ( { post }) => {
     if(router.isFallback) {
       return <Loader/>
     }
+
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
