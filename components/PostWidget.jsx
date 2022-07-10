baca06ee907e0abc1af4ae6bb92bf21ae7c32019
Chakrit Thong Ek <thongekchakrit@gmail.com>
2022-07-10 18:24:14 +0800

    completed blog

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
 const PostWidget = () => {
   return (
     <div>
-    PostWidget
+
     </div>
   )
 }
--- services/index.js
+++ services/index.js
@@ -34,7 +34,29 @@ export const getPosts = async ( ) => {
       
     `
 
-    const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query)
 
-    return result.postsConnection.edges;
+  return result.postsConnection.edges;
+}
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last:3
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+      
+    }
+  `
+  const result = await request(graphqlAPI, query)
+
+  return result.postsConnection.edges;
 }
