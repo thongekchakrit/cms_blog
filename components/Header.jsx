f741c25f3001834dda9d5b058a2be9205b36c0b1
Chakrit Thong Ek <thongekchakrit@gmail.com>
2022-07-11 13:53:25 +0800

    updated header

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,11 +11,11 @@ const Header = () => {
   }, []);
 
   return (
-    <div className='mx-auto px-20 mb-8 mt-0  bg-gray-900'> 
-        <div className='w-full inline-block py-4'>
+    <div className='container mx-auto px-10 mb-8 mt-0'> 
+        <div className='border-b w-full inline-block py-4 border-gray-300'>
             <div className='md:float-left block'> 
                 <Link href="/">
-                    <span className="cursor-pointer font-semibold text-2xl text-white">
+                    <span className="cursor-pointer font-semibold text-2xl text-gray-900">
                     💻 Chakrit Coding Blog
                     </span>
                 </Link>
@@ -23,7 +23,7 @@ const Header = () => {
             <div className="hidden md:float-left md:contents">
                 {categories.map((categories) => (
                     <Link key={categories.slug} href={`/category/${categories.slug}`}>
-                        <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                        <span className="md:float-right mt-2 align-middle text-gray-900 ml-4 font-semibold cursor-pointer">
                             {categories.name}
                         </span>
                     </Link>
